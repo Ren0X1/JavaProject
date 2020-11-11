15ec4032d059d716d68143229d8356708e5136b3
Ren0X <alexyanamusicpro@gmail.com>
2020-11-11 10:34:27 +0100

    Añadido una clase para usarla como utilidad para trabajar con parametros pasados por linea de comandos

---
+++ src/Args.java
@@ -0,0 +1,54 @@
+import java.util.*;//Importamos todo el java util ya qu es una clase de utilidad
+public class Args {
+    //Metodo para comprobar los numeros de parametros, el maximo s lo pasamos por parametro
+    public static boolean ComprobrarLongitud(String[] argumentos,int n) {
+        if (argumentos.length!=n){return false;}//Devuelve false si no se han colocado todos los args
+        return true;//Sino devuelve true
+    }
+    //Con esto comprobamos si los argumentos son de tipo entero o otro tipo
+    public static boolean ComprobrarTipoArgs(String[] argumentos) {
+        int c=0;
+        for (int j=0;j!=argumentos.length;j++) {
+            try {
+                Integer.parseInt(argumentos[j]);
+                c++;
+            } catch(NumberFormatException e){
+                //No es un entero :(
+            }
+        }
+        if (c==argumentos.length) {
+            return true;//Si tenemos la longitud igual al contador los argumentos eran todos de tipo int
+        }
+        return false;// Sino devolvemos false
+    }
+    //Con esto comprobamos que tipo de entero es, si negativo o positivo
+    public static int ComprobrarTipoEntero(String[] argumentos,int n) {
+        int i=Integer.parseInt(argumentos[n]);
+        switch (RangoNumerico.from(i)) {
+            case ENTERO:
+                return 1;//Si es positivo devolvemos 1
+            case NEGATIVO:// Si es negativo devolvemos 2
+                return 2;
+            default:
+                return 0;// Si por algun caso el metodo falla devolvemos 0
+        }
+    }
+    //Metodo para crear los rangos numericos para comprobarlos con el metodo de @ComprobarTipoEntero
+    public enum RangoNumerico {
+        ENTERO(0, 32000),
+        NEGATIVO(-32000, -1),
+        OTRO(0, -1);
+        private final int minValue;
+        private final int maxValue;
+        RangoNumerico(int min, int max) {
+            this.minValue = min;
+            this.maxValue = max;
+        }
+        public static RangoNumerico from(int score) {
+            return Arrays.stream(RangoNumerico.values())
+                    .filter(range -> score >= range.minValue && score <= range.maxValue)
+                    .findAny()
+                    .orElse(OTRO);
+        }
+    }
+}
