1739097d6de31cb491b5a13f73847f89bcea5b39
Alejandro <alexyanamusicpro@gmail.com>
2020-12-10 09:26:36 +0100

    Añadido metodo para generar apuesta pasando por parametro la longitud del numero y los digitos de los numeros generados

--- src/Args.java
+++ src/Args.java
@@ -44,6 +44,38 @@ public class Args {
             if(a!=numeros.length-1){System.out.print(", ");}
         }
     }
+    public static double aleatorio(int n) {
+        int min,max;
+        switch (n) {
+            case 1:
+                min=1;
+                max=9;
+                break;
+            case 2:
+                min=10;
+                max=99;
+                break;
+            case 3:
+                min=100;
+                max=999;
+                break;
+            case 4:
+                min=1000;
+                max=9999;
+                break;
+            default:
+                min=1;
+                max=32000;
+        }
+        return min+Math.floor(Math.random()*(max-min)+1);
+    }
+    public static int[] GenerarApuesta(int longitud, int numeros) {
+        int[] apuesta=new int[longitud];
+        for (int i=0;i!= apuesta.length;i++) {
+           apuesta[i]=(int)aleatorio(numeros);
+        }
+        return apuesta;
+    }
     //Metodo para ordenar una array
     public static void Ordenar(int[] array) {
         Arrays.sort(array);
---
+++ src/Combinacion.java
@@ -0,0 +1,34 @@
+public class Combinacion {
+    static double aleatorio(int n) {
+        int min,max;
+        switch (n) {
+            case 1:
+                min=1;
+                max=9;
+                break;
+            case 2:
+                min=10;
+                max=99;
+                break;
+            case 3:
+                min=100;
+                max=999;
+                break;
+            case 4:
+                min=1000;
+                max=9999;
+                break;
+            default:
+                min=1;
+                max=32000;
+        }
+        return min+Math.floor(Math.random()*(max-min)+1);
+    }
+    static int[] GenerarApuesta(int longitud, int numeros) {
+        int[] apuesta=new int[longitud];
+        for (int i=0;i!= apuesta.length;i++) {
+            apuesta[i]=(int)aleatorio(numeros);
+        }
+        return apuesta;
+    }
+}
