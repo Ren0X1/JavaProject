fdfc24b0cea90dff9af411eb80dc5d856454ebdb
Alejandro <alexyanamusicpro@gmail.com>
2020-12-11 13:44:57 +0100

    Añadido metodo para generar apuestas del estado y mejorada la generacion de numeros en Combinacion.class

--- src/Combinacion.java
+++ src/Combinacion.java
@@ -1,43 +1,18 @@
 public class Combinacion {
-    public static double aleatorio(int n) {
-        int min,max;
-        switch (n) {
-            case 1:
-                min=1;
-                max=9;
-                break;
-            case 2:
-                min=10;
-                max=99;
-                break;
-            case 3:
-                min=100;
-                max=999;
-                break;
-            case 4:
-                min=1000;
-                max=9999;
-                break;
-            default:
-                min=1;
-                max=32000;
-        }
+    public static double aleatorio(double min, double max) {
         return min+Math.floor(Math.random()*(max-min)+1);
-    }
-    public static double aleatorio(int min, int max) {
-        return min+Math.floor(Math.random()*(max-min)+1);
-    }
-    public static int[] GenerarApuesta(int longitud, int numeros) {
-        int[] apuesta=new int[longitud];
-        for (int i=0;i!= apuesta.length;i++) {
-            apuesta[i]=(int)aleatorio(numeros);
-        }
-        return apuesta;
     }
     public static int[] GenerarApuesta(int longitud, int min,int max) {
         int[] apuesta=new int[longitud];
         for (int i=0;i!= apuesta.length;i++) {
             apuesta[i]=(int)aleatorio(min,max);
+        }
+        return apuesta;
+    }
+    public static double[] GenerarApuesta(int longitud, double min,double max) {
+        double[] apuesta=new double[longitud];
+        for (int i=0;i!= apuesta.length;i++) {
+            apuesta[i]=aleatorio(min,max);
         }
         return apuesta;
     }
